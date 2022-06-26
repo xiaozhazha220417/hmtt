2eece826b75fd0734916d490092243dbd6b84cb2
xiaozhazha220417 <2053678428@qq.com>
2022-06-26 11:44:24 +0800

    路由配置完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
+// 项目初始化包
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// vant 组件包
 import Vant from 'vant'
 import 'vant/lib/index.css'
-
+// 响应式第三方包
 import 'amfe-flexible'
+// 导入字体图标
+import '@/styles/icon.less'
 
 Vue.use(Vant)
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Layout from '@/views/Layout'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        redirect: 'home'
+      },
+      {
+        path: 'home',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: 'question',
+        component: () => import('@/views/Question')
+      },
+      {
+        path: 'video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: 'my',
+        component: () => import('@/views/My')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/Login')
+  }
 ]
 
 const router = new VueRouter({
